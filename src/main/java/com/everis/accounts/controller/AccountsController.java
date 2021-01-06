b82d2a9c53037f5a674f695ee8bc0d820efc40d0
Valeasmat <valeriaasmat2dob@gmail.com>
2021-01-06 14:09:14 -0500

    swagger config

--- src/main/java/com/everis/accounts/controller/AccountsController.java
+++ src/main/java/com/everis/accounts/controller/AccountsController.java
@@ -5,6 +5,8 @@ import com.everis.accounts.dto.Account;
 import com.everis.accounts.dto.AccountResponse;
 import com.everis.accounts.service.AccountsService;
 import io.reactivex.Single;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +21,10 @@ public class AccountsController {
     @Autowired
     private AccountsService accountsService;
 
+    @ApiOperation(value = "Get request to retrieve client's accounts",
+            response = AccountResponse.class,
+            produces = "application/stream+json")
+    @ApiResponse(code = 200,message = "Successful operation",response = AccountResponse.class)
     @GetMapping(value = "/core/accounts",produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Single<AccountResponse> getAccount(@RequestParam("cardNumber") String cardNumber){
         Single<Account> accountByCardNumber = accountsService.getAccount(cardNumber);
--- src/main/java/com/everis/accounts/dto/AccountResponse.java
+++ src/main/java/com/everis/accounts/dto/AccountResponse.java
@@ -1,5 +1,7 @@
 package com.everis.accounts.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 @Setter
@@ -7,7 +9,10 @@ import lombok.*;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@ApiModel("Account response")
 public class AccountResponse {
+    @ApiModelProperty(example = "1111222233334441XXX")
     private String accountNumber;
+    @ApiModelProperty(example = "1000")
     private Long amount;
 }
